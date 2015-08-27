ac0f5d29085282382906ccb07f91ef46a09a2bed
Kodi123 <Kodi.Brunson@gmail.com>
2015-08-26 21:47:59 -0500

    Testing Debug Class

--- src/main.java
+++ src/main.java
@@ -1,6 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class main {
     public static void main( String[] args ) {
-      System.out.println("Hello World!");
+    	Debugging.DebugPrint( "Hello World!" );
+    	List<String> Messages = new ArrayList<String>();
+    	Messages.add("Hello");
+    	Messages.add("World");
+    	Messages.add("!");
+    	
+    	Debugging.DebugTable(Messages, "Debug Table Test");
 
     }
 
