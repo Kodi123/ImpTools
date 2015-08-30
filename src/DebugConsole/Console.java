7c29086e3f45d8a7f7544824788af7344c2503f4
Kodi123 <Kodi.Brunson@gmail.com>
2015-08-30 12:03:28 -0500

    Finished up DebugConsole
    * Removed Logging.java and compounded into Console.Java

--- src/DebugConsole/Console.java
+++ src/DebugConsole/Console.java
@@ -2,40 +2,45 @@ package DebugConsole;
 
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
 import javax.swing.border.EmptyBorder;
+import javax.swing.text.StyledDocument;
 
 public class Console extends JFrame {
 
 	private String title = "Debug Console";
 	private int width = 400;
-	private int height = ( width / 16 ) * 9;
-	
+	private int height = (width / 16) * 9;
+
 	private JPanel contentPane;
 	private JTextPane log;
 	private GridBagConstraints gbc_log;
 	private GridBagLayout layout;
+	private StyledDocument doc;
+	
 	
 	public Console() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(0, 0, width, height);
 		setTitle(title);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(0, 0, 30, 0)); //Top, Left, Bottom, Right
+		contentPane.setBorder(new EmptyBorder(0, 0, 30, 0)); // Top, Left,
+																// Bottom, Right
 		setContentPane(contentPane);
-		
+
 		layout = new GridBagLayout();
-		layout.columnWidths = new int[]{0, 0};
-		layout.rowHeights = new int[]{0, 0};
-		layout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		layout.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		layout.columnWidths = new int[] { 0, 0 };
+		layout.rowHeights = new int[] { 0, 0 };
+		layout.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
+		layout.rowWeights = new double[] { 1.0, Double.MIN_VALUE };
 		contentPane.setLayout(layout);
-		
+
 		log = new JTextPane();
-		log.setEditable(false);
+		doc = log.getStyledDocument();
 		gbc_log = new GridBagConstraints();
 		gbc_log.fill = GridBagConstraints.BOTH;
 		gbc_log.gridx = 0;
@@ -43,4 +48,35 @@ public class Console extends JFrame {
 		contentPane.add(log, gbc_log);
 	}
 	
+	public void DebugPrint( String Message ) {
+		try {
+			doc.insertString(doc.getLength(), "Debug: " + Message + "\n", null );
+		} catch(Exception e) { 
+			System.out.println(e); 
+		}
+	}
+	
+	public void DebugTable( ArrayList<String> messages, String Iterator ) {
+		String CheckIterator = ParseIterator(Iterator);
+		for( int i = 0; i < messages.size(); i++ ) {
+			if( CheckIterator == "Numbers" ) {
+				DebugPrint(i + " - " + messages.get(i) );
+			} else {
+				DebugPrint( CheckIterator + " - " + messages.get(i) );
+			}
+		}
+	}
+	
+	private String ParseIterator( String Iterator ) {
+		switch( Iterator ) {
+		case("+"):
+			return "+";
+		case("#"):
+			return "Numbers";
+		default:
+			return Iterator;
+		
+		}
+	}
+
 }
