342209c7521ffee2b619a3e2237a1f20128e487b
Kodi123 <Kodi.Brunson@gmail.com>
2015-08-30 11:36:40 -0500

    Refactored Main for more consistency

--- src/main.java
+++ src/main.java
@@ -1,14 +1,11 @@
 import DebugConsole.Console;
 import DebugConsole.Logging;
 
-public class main {
+public class Main {
     public static void main( String[] args ) {
     	Console console = new Console();
     	console.setVisible(true);
     	Logging.DebugPrint( "Hello World!" );
-    		
-
+    	
     }
-
-
 }
